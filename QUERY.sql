92e6bdb9acd59a9b0f36787869f656cb3ab82858
shafayat783593 <sshapa17@gmail.com>
2026-06-15 00:01:07 +0600

    filter booking above average total cost using subquery

--- QUERY.sql
+++ QUERY.sql
@@ -18,8 +18,18 @@ select booking_id,user_id,match_id, coalesce(payment_status,'Action Required') a
   from bookings where payment_status is null 
 
   -- Query 4: Answer
-  
+
   select b.booking_id, u.full_name, m.fixture ,b.total_cost
   from matches m 
   inner  join bookings b  using(match_id) 
 inner join users u  using(user_id)
+
+-- Query 5: Answer
+
+select u.user_id ,u.full_name, b.booking_id from users  u left join bookings b using(user_id)
+
+-- Query 6: Answer
+
+select booking_id,match_id,total_cost from  bookings  where  total_cost > (
+ select round(avg(total_cost)) from bookings
+)
